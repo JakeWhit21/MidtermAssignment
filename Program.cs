6f18b10558033ed3ab98ab49eeac53682aac0f7b
Jake <jdwhitmore3@gmail.com>
2023-03-13 15:14:13 -0500

    Completed add bug ticket functionality

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-string file = "Tickets.txt";
+string bugFile = "BugTickets.txt";
+string enhancementsFile = "EnhancementsTickets.txt";
+string taskFile = "TaskTickets.txt";
 string choice;
+string ticketChoice;
 
 TicketsFile ticketsFile = new TicketsFile();
 
@@ -23,16 +26,25 @@ do
 
     if (choice == "1")
     {
-        ticketsFile.ReadTicket(file);
-        foreach (Ticket t in ticketsFile.tickets)
+        Console.WriteLine("Which ticket do you want to enter?");
+        Console.WriteLine("1) Bug");
+        Console.WriteLine("2) Enhancement");
+        Console.WriteLine("3) Task");
+        ticketChoice = Console.ReadLine();
+
+        if(ticketChoice == "1")
         {
-            Console.WriteLine(t.Display());
+        ticketsFile.ReadBugTicket(bugFile);
+        foreach (Bug b in ticketsFile.tickets)
+        {
+            Console.WriteLine(b.Display());
         }
+        }
+
+        
     }
     else if (choice == "2")
     {
-        string ticketChoice;
-
         Console.WriteLine("Which ticket do you want to enter?");
         Console.WriteLine("1) Bug");
         Console.WriteLine("2) Enhancement");
@@ -41,34 +53,60 @@ do
 
         if (ticketChoice == "1")
         {
-            
+            Bug bug = new Bug();
+
+            Console.WriteLine("Enter ticket ID");
+        bug.ticketId = Console.ReadLine();
+
+        Console.WriteLine("Enter the summary for the ticket: ");
+        bug.summary = Console.ReadLine();
+
+        Console.WriteLine("Enter status: ");
+        bug.status = Console.ReadLine();
+
+        Console.WriteLine("Enter priority: ");
+        bug.priority = Console.ReadLine();
+
+        Console.WriteLine("Enter submitter: ");
+        bug.submitter = Console.ReadLine();
+
+        Console.WriteLine("Enter person assigned to: ");
+        bug.assigned = Console.ReadLine();
+
+        Console.WriteLine("Enter person watching: ");
+        bug.watching = Console.ReadLine();
+
+        Console.WriteLine("Enter severity: ");
+        bug.severity = Console.ReadLine();
+
+        ticketsFile.AddBug(bug);
         }
 
 
-        Ticket ticket = new Ticket();
+        // Ticket ticket = new Ticket();
 
-        Console.WriteLine("Enter ticket ID");
-        ticket.ticketId = Console.ReadLine();
+        // Console.WriteLine("Enter ticket ID");
+        // ticket.ticketId = Console.ReadLine();
 
-        Console.WriteLine("Enter the summary for the ticket: ");
-        ticket.summary = Console.ReadLine();
+        // Console.WriteLine("Enter the summary for the ticket: ");
+        // ticket.summary = Console.ReadLine();
 
-        Console.WriteLine("Enter status: ");
-        ticket.status = Console.ReadLine();
+        // Console.WriteLine("Enter status: ");
+        // ticket.status = Console.ReadLine();
 
-        Console.WriteLine("Enter priority: ");
-        ticket.priority = Console.ReadLine();
+        // Console.WriteLine("Enter priority: ");
+        // ticket.priority = Console.ReadLine();
 
-        Console.WriteLine("Enter submitter: ");
-        ticket.submitter = Console.ReadLine();
+        // Console.WriteLine("Enter submitter: ");
+        // ticket.submitter = Console.ReadLine();
 
-        Console.WriteLine("Enter person assigned to: ");
-        ticket.assigned = Console.ReadLine();
+        // Console.WriteLine("Enter person assigned to: ");
+        // ticket.assigned = Console.ReadLine();
 
-        Console.WriteLine("Enter person watching: ");
-        ticket.watching = Console.ReadLine();
+        // Console.WriteLine("Enter person watching: ");
+        // ticket.watching = Console.ReadLine();
 
-        ticketsFile.AddTicket(ticket);
+        // ticketsFile.AddTicket(ticket);
     }
 } while (choice == "1" || choice == "2");
 
--- TicketsFile.cs
+++ TicketsFile.cs
@@ -10,9 +10,8 @@ public class TicketsFile
         
     }
 
-    public void ReadTicket(string ticketFile)
+    public void ReadBugTicket(string ticketFile)
     {
-        filePath = ticketFile;
 
         tickets = new List<Ticket>();
         // read data from file
@@ -22,21 +21,22 @@ public class TicketsFile
             StreamReader sr = new StreamReader(ticketFile);
             while (!sr.EndOfStream)
             {
-                Ticket ticket = new Ticket();
+                Bug bugTicket = new Bug();
                 string line = sr.ReadLine();
                 //convert string to array
                 string[] ticketDetails = line.Split('|');
                 //display array data
                 //Console.WriteLine("TicketID: {0}, Summary: {1}, Status: {2}, Priority: {3}, Submitter: {4}, Assigned: {5}, Watching: {6}", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
-                ticket.ticketId = ticketDetails[0];
-                ticket.summary = ticketDetails[1];
-                ticket.status = ticketDetails[2];
-                ticket.priority = ticketDetails[3];
-                ticket.submitter = ticketDetails[4];
-                ticket.assigned = ticketDetails[5];
-                ticket.watching = ticketDetails[6];
+                bugTicket.ticketId = ticketDetails[0];
+                bugTicket.summary = ticketDetails[1];
+                bugTicket.status = ticketDetails[2];
+                bugTicket.priority = ticketDetails[3];
+                bugTicket.submitter = ticketDetails[4];
+                bugTicket.assigned = ticketDetails[5];
+                bugTicket.watching = ticketDetails[6];
+                bugTicket.severity = ticketDetails[7];
 
-                tickets.Add(ticket);
+                tickets.Add(bugTicket);
             }
         
             sr.Close();
@@ -52,6 +52,14 @@ public class TicketsFile
         filePath = "Tickets.txt";
         StreamWriter sw = new StreamWriter(filePath, true);
         sw.WriteLine($"{ticket.ticketId}|{ticket.summary}|{ticket.status}|{ticket.priority}|{ticket.submitter}|{ticket.assigned}|{ticket.watching}");
+        sw.Close();
+    }
+
+    public void AddBug(Bug bug)
+    {
+        filePath = "BugTickets.txt";
+        StreamWriter sw = new StreamWriter(filePath, true);
+        sw.WriteLine($"{bug.ticketId}|{bug.summary}|{bug.status}|{bug.priority}|{bug.submitter}|{bug.assigned}|{bug.watching}|{bug.severity}");
         sw.Close();
     }
 }
